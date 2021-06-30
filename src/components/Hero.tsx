e687007d670b89ad82fadbcda069b36503b7371e
Revai Nandor <revnandi@gmail.com>
2021-06-30 18:46:09 +0200

    add hero video partly

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,6 +4,15 @@ import HeroItem from './HeroItem';
 
 function Hero() {
   return <div className={ classes.Hero }>
+    <div className={ classes.VimeoWrapper }>
+      <iframe
+        className={ classes.Iframe   }
+        src='https://player.vimeo.com/video/541719840?autoplay=1&loop=1&title=0&byline=0&portrait=0?background=1?muted=1'
+        frameBorder='false'
+        data-allow='autoplay; fullscreen; picture-in-picture'
+        allowFullScreen={true}>
+      </iframe>
+    </div>
     <HeroItem></HeroItem>
   </div>
 }
