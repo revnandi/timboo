a617bf79b9a1a594db0c701c6d54613240e029e6
Revai Nandor <revnandi@gmail.com>
2021-07-20 00:11:19 +0200

    add staff text toggle logic

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -9,13 +9,18 @@ type ImageProps = {
   lqip?: string,
   alt?: string,
   width: string,
-  height: string
+  height: string,
+  squared?: boolean
 }
 
-const Image = React.forwardRef<HTMLImageElement, ImageProps>(({ src, lqip, alt, width, height }, ref) => {
-  return <div className={ classes.Container }>
-    <img ref={ref} className={ [classes.Image, 'lazyload', 'blur-up' ].join(' ') } src={ lqip } data-src={ src } alt={ alt } width={ width } height={ height } />
+const Image = React.forwardRef<HTMLImageElement, ImageProps>(({ src, lqip, alt, width, height, squared }, ref) => {
+  return <div className={ [classes.Container, squared ? classes.SquareContainer : ''].join(' ') }>
+    <img ref={ref} className={ [classes.Image, 'lazyload', 'blur-up', (squared ? classes.SquareImage : '') ].join(' ') } src={ lqip } data-src={ src } alt={ alt } width={ width } height={ height }/>
   </div>
 });
+
+Image.defaultProps = {
+  squared: false
+}
 
 export default Image;
--- src/components/StaffGroupItem.tsx
+++ src/components/StaffGroupItem.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import classes from '../scss/StaffGroupItem.module.scss';
 import Image from './Image';
 import { createMarkup } from '../Helpers'
@@ -18,26 +18,65 @@ type StaffMember = {
 
 const StaffGroupItem = ({ title, content, featuredImage }: StaffMember) => {
 
+  const [opened, setOpened] = useState(false);
+  const [contentHeight, setContentHeight] = useState(0);
+
   const imageRef = useRef<HTMLImageElement>(null);
   const nameRef = useRef<HTMLHeadingElement>(null);
   const contentRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const imageHeight = imageRef?.current?.offsetHeight;
-    const nameHeight = nameRef?.current?.offsetHeight;
-    const contentHeight = contentRef?.current?.offsetHeight;
 
-    console.log('Image height', imageHeight);
-    console.log('Name height', nameHeight);
-    console.log('Content height', contentHeight);
-    
-    if(contentRef && contentRef.current && imageHeight && nameHeight) {
-      const maxContentHeight = (imageHeight - nameHeight).toString() + 'px';
-      console.log(maxContentHeight);
-      contentRef.current.style.maxHeight = maxContentHeight;
+    let imageHeight = imageRef?.current?.offsetHeight;
+    let nameHeight = nameRef?.current?.offsetHeight;
+    let contentHeight = contentRef?.current?.offsetHeight;
+
+    if(contentHeight) {
+      setContentHeight(contentHeight);
     }
 
+    const handleResize = () => {
+      imageHeight = imageRef?.current?.offsetHeight;
+      nameHeight = nameRef?.current?.offsetHeight;
+      contentHeight = contentRef?.current?.offsetHeight;
+
+      if(contentHeight) {
+        setContentHeight(contentHeight);
+      }
+
+      setContentMaxHeight();
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    const setContentMaxHeight = () => {
+      if(contentRef && contentRef.current && imageHeight && nameHeight) {
+        const maxContentHeight = (imageHeight - nameHeight).toString() + 'px';
+        contentRef.current.style.maxHeight = maxContentHeight;
+      }
+    }
+
+    setContentMaxHeight();
+
   }, [imageRef, nameRef, contentRef]);
+
+  const toggleContentAutoHeight = () => {
+      if(contentRef && contentRef.current) {
+
+        if(opened) {
+          contentRef.current.style.maxHeight = contentHeight.toString() + 'px';
+          contentRef.current.style.paddingBottom = '0.75rem';
+        } else {
+          contentRef.current.style.maxHeight = '999999999px';
+          contentRef.current.style.paddingBottom = '0.75rem';
+        }
+      };
+  };
+
+  const clickHandler = () => {
+    toggleContentAutoHeight();
+    setOpened(!opened);
+  }
 
   return <li className={ classes.Body }>
     <Image
@@ -46,10 +85,15 @@ const StaffGroupItem = ({ title, content, featuredImage }: StaffMember) => {
       data-src={ featuredImage.lqip.src }
       width='320'
       height='320'
+      squared={ true }
     />
     <div className={ classes.InnerContainer }>
       <h2 ref={ nameRef } className={ classes.Name }>{ title}</h2>
-      <div ref={ contentRef } className={ classes.Content } dangerouslySetInnerHTML={ createMarkup(content) }></div>
+      <div ref={ contentRef } className={ classes.Content } dangerouslySetInnerHTML={ createMarkup(content) }>
+      </div>
+      <div className={ classes.Button } onClick={ clickHandler }>
+        <span className={ [classes.ButtonIcon, opened ? classes.FlippedButtonIcon : ''].join(' ') }></span>
+      </div>
     </div>
   </li>
 }
